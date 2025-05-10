a94a6f878ded96a4cd61f4084cb22ba7c7bcb5b9
Cornichon <antoni.blanche@proton.me>
2025-05-11 01:03:57 +0200

    FD list refactor

--- libcvb/cvb/fdlist.h
+++ libcvb/cvb/fdlist.h
@@ -1,24 +1,72 @@
+/*
+ * Functions dealing with file descriptors lists.
+ * Copyright (c) 2025 Antoni Blanche
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #ifndef FDLIST_H
 #define FDLIST_H
 
 #include <poll.h>
 
+/*
+ * File descriptors list initializer.
+ */
 #define FDLIST_INIT {NULL, 0, 0}
 
-#define fdl_array(fdl) (fdl.fds)
-#define fdl_lenght(fdl) (fdl.cursor)
-#define fdl_get(fdl, i) (fdl.fds[i])
-
+/*
+ *
+ */
 struct fdlist {
         struct pollfd *fds;
         nfds_t size;
         nfds_t cursor;
 };
 
+/*
+ * Return a file descriptors list as an array.
+ */
+#define fdl_array(fdl) (fdl.fds)
+
+/*
+ * Return the lenght of a file descriptors list.
+ */
+#define fdl_lenght(fdl) (fdl.cursor)
+
+/*
+ * Return a file descriptor from a list.
+ */
+#define fdl_get(fdl, i) (fdl.fds[i])
+
+/*
+ * Add a file descriptor in a list.
+ */
 int fdl_add(struct fdlist *fdl, int fd);
 
+/*
+ * Remove a file descriptor from a list.
+ */
 int fdl_remove(struct fdlist *fdl, int fd);
 
+/*
+ * Destroy a file descriptors list.
+ */
 void fdl_destroy(struct fdlist *fdl);
 
 #endif /* fdlist.h */
--- libcvb/fdlist.c
+++ libcvb/fdlist.c
@@ -1,9 +1,48 @@
+/*
+ * Functions dealing with file descriptors lists.
+ * Copyright (c) 2025 Antoni Blanche
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include <stdlib.h>
 
 #include <cvb/fdlist.h>
 
+/*
+ * Default file descriptors list size.
+ */
 #define DEFAULT_SIZE 10UL
 
+/*
+ * Apply a left shift on a file descriptors list.
+ */
+static void fdl_left_shift(struct fdlist *const fdl, const nfds_t start)
+{
+        nfds_t i;
+
+        for (i = start; i < fdl->size - 1; ++i)
+                fdl->fds[i] = fdl->fds[i + 1];
+}
+
+/*
+ * Add a file descriptor in a list.
+ */
 int fdl_add(struct fdlist *const fdl, const int fd)
 {
         if (fdl->fds == NULL) {
@@ -35,17 +74,17 @@ int fdl_add(struct fdlist *const fdl, const int fd)
         return 0;
 }
 
+/*
+ * Remove a file descriptor from a list.
+ */
 int fdl_remove(struct fdlist *const fdl, const int fd)
 {
-        nfds_t i, j;
+        nfds_t i;
 
         for (i = 0; i < fdl->size; ++i) {
                 if (fdl->fds[i].fd == fd) {
-                        for (j = i; j < fdl->size - 1; ++j)
-                                fdl->fds[j] = fdl->fds[j + 1];
-
+                        fdl_left_shift(fdl, i);
                         --fdl->cursor;
-
                         return 0;
                 }
         }
@@ -53,6 +92,9 @@ int fdl_remove(struct fdlist *const fdl, const int fd)
         return -1;
 }
 
+/*
+ * Destroy a file descriptors list.
+ */
 void fdl_destroy(struct fdlist *const fdl)
 {
         free(fdl->fds);
