36b5c7192ab0c7729e17d02421ba918aaabc87ba
Cornichon <antoni.blanche@proton.me>
2025-06-03 14:45:18 +0200

    Time for documentation

--- libcvb/cvb/net.h
+++ libcvb/cvb/net.h
@@ -1,12 +1,87 @@
+/**
+ * \file       net.h
+ * \brief      Functions dealing with network connections
+ *
+ * Copyright (c) 2025 Antoni Blanche
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #ifndef CVB_NET_H
 #define CVB_NET_H
 
+/**
+ * \brief      Fetch a socket.
+ *
+ * The \c fetch_socket() function tries to fetch a socket with the given
+ * parameters \a host and \a service. If the \a host is NULL, then
+ * \c fetch_socket() will return a socket suitable for \c clnt_connect().
+ * Otherwise, the socket will be \c connect()ed directly to the \a host.
+ *
+ * \param[in]  host     The host
+ * \param[in]  service  The service
+ *
+ * \return     The fetched socket
+ */
 int fetch_socket(const char *host, const char *service);
 
+/**
+ * \brief      Accept a new client connection
+ *
+ * The \c clnt_connect() function tries to \c accept() a new connection request
+ * from the listening socket \a listener and return the new connected socket.
+ * The argument \a listener must be a socket that has been created with
+ * \c fetch_socket().
+ *
+ * \see fetch_socket()
+ *
+ * \param[in]  listener  The listening socket
+ *
+ * \return     The new socket on success, -1 otherwise
+ */
 int clnt_connect(int listener);
 
+/**
+ * \brief      Send a message
+ *
+ * The \c send_msg() function sends up to \a size bytes from the message \a msg
+ * to the socket \a sfd.
+ *
+ * \param[in]  sfd   The socket
+ * \param[in]  msg   The message
+ * \param[in]  size  The message size
+ *
+ * \return     the number of bytes written
+ */
 int send_msg(int sfd, const void *msg, size_t size);
 
+/**
+ * \brief      Read a message
+ *
+ * The \c read_msg() function reads up to \a size bytes from the socket \a sfd
+ * and stores them into the message \a msg.
+ *
+ * \param[in]  sfd   The socket
+ * \param[out] msg   The message
+ * \param[in]  size  The message size
+ *
+ * \return     the number of bytes read
+ */
 int read_msg(int sfd, void *msg, size_t size);
 
 #endif /* cvb/net.h */
--- libcvb/net.c
+++ libcvb/net.c
@@ -1,3 +1,27 @@
+/**
+ * \file       net.c
+ * \brief      Functions dealing with network connections
+ *
+ * Copyright (c) 2025 Antoni Blanche
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
@@ -11,8 +35,15 @@
 
 #include <logger.h>
 
-/*
- * Try each address in the list until successfully bind a socket.
+/**
+ * \brief      Bind a socket
+ *
+ * The \c bind_socket() function tries to \c bind() each socket provided by
+ * the address list \a rp until success.
+ *
+ * \param[in]  rp    The address list
+ *
+ * \return     The binded socket
  */
 static int bind_socket(const struct addrinfo *rp)
 {
@@ -35,8 +66,15 @@ static int bind_socket(const struct addrinfo *rp)
         return -1;
 }
 
-/*
- * Try each address in the list until successfully connect a socket.
+/**
+ * \brief      Connect a socket
+ *
+ * The \c connect_socket() function tries to \c connect() each socket provided
+ * by the address list \a rp until success.
+ *
+ * \param[in]  rp    The address list
+ *
+ * \return     The Connected socket
  */
 static int connect_socket(const struct addrinfo *rp)
 {
@@ -56,8 +94,18 @@ static int connect_socket(const struct addrinfo *rp)
         return -1;
 }
 
-/*
- * Fetch a socket and bind it.
+/**
+ * \brief      Fetch a socket.
+ *
+ * The \c fetch_socket() function tries to fetch a socket with the given
+ * parameters \a host and \a service. If the \a host is NULL, then
+ * \c fetch_socket() will return a socket suitable for \c clnt_connect().
+ * Otherwise, the socket will be \c connect()ed directly to the \a host.
+ *
+ * \param[in]  host     The host
+ * \param[in]  service  The service
+ *
+ * \return     The fetched socket
  */
 int fetch_socket(const char *const host, const char *const service)
 {
@@ -110,8 +158,19 @@ int fetch_socket(const char *const host, const char *const service)
         return sfd;
 }
 
-/*
- * Accept a new client connection.
+/**
+ * \brief      Accept a new client connection
+ *
+ * The \c clnt_connect() function tries to \c accept() a new connection request
+ * from the listening socket \a listener and return the new connected socket.
+ * The argument \a listener must be a socket that has been created with
+ * \c fetch_socket().
+ *
+ * \see fetch_socket()
+ *
+ * \param[in]  listener  The listening socket
+ *
+ * \return     The new socket on success, -1 otherwise
  */
 int clnt_connect(const int listener)
 {
@@ -142,6 +201,18 @@ int clnt_connect(const int listener)
         return sfd;
 }
 
+/**
+ * \brief      Send a message
+ *
+ * The \c send_msg() function sends up to \a size bytes from the message \a msg
+ * to the socket \a sfd.
+ *
+ * \param[in]  sfd   The socket
+ * \param[in]  msg   The message
+ * \param[in]  size  The message size
+ *
+ * \return     the number of bytes written
+ */
 int send_msg(const int sfd, const void *const msg, const size_t size)
 {
         int nsent;
@@ -154,8 +225,17 @@ int send_msg(const int sfd, const void *const msg, const size_t size)
         return nsent;
 }
 
-/*
- * Receive a message from a connected client.
+/**
+ * \brief      Read a message
+ *
+ * The \c read_msg() function reads up to \a size bytes from the socket \a sfd
+ * and stores them into the message \a msg.
+ *
+ * \param[in]  sfd   The socket
+ * \param[out] msg   The message
+ * \param[in]  size  The message size
+ *
+ * \return     the number of bytes read
  */
 int read_msg(const int sfd, void *const msg, const size_t size)
 {
